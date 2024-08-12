88942597b2e19096219cf3574f1c706fa11b7f01
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-12 06:25:28 -0300

    added tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1 className="text-7xl">Welcome</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
