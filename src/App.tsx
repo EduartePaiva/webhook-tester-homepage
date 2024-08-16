6965ab002b6e84408bf7222befa54657e11b1fbf
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-16 07:22:20 -0300

    chosed the icon logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Webhook } from "lucide-react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 
 function App() {
     return (
-        <div className="bg-background">
-            <header className="flex items-center gap-2 mx-6 my-2">
-                <span className="font-semibold text-lg mr-auto">Webhook</span>
+        <div className="container">
+            <header className="flex items-center gap-2 my-2">
+                <span className="font-bold text-lg mr-auto flex gap-2">
+                    <Webhook />
+                    webhook
+                </span>
                 <Button className="rounded-full" variant={"ghost"}>
                     Sign in
                 </Button>
